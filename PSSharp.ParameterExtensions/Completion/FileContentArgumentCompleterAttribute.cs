a773afbe7b3ce9e82d77c9f16c591c4cebed8bed
Caleb Frederickson <CFrederickson@KPBSD.k12.ak.us>
2021-03-29 07:27:50 -0800

    implement file content argument completer

---
+++ PSSharp.ParameterExtensions/Completion/FileContentArgumentCompleterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using PSSharp.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+
+namespace PSSharp
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class FileContentArgumentCompleterAttribute : ArgumentCompleterAttribute
+    {
+        public FileContentArgumentCompleterAttribute(string filePath)
+            : base(GetScript(filePath))
+        {
+        }
+        private static ScriptBlock GetScript(string filePath)
+        {
+            var scriptText = string.Format(Resources.FileContentArgumentCompleter, filePath?.Replace("\"", "\"\""));
+            return ScriptBlock.Create(scriptText);
+        }
+    }
+}
---
+++ PSSharp.ParameterExtensions/Resources/FileContentArgumentCompleter.ps1
@@ -0,0 +1,46 @@
+[CmdletBinding()]
+param(
+	[Parameter()]
+	[System.String]
+	$commandName,
+	
+	[Parameter()]
+	[System.String]
+	$parameterName,
+	
+	[Parameter()]
+	[System.String]
+	$wordToComplete,
+	
+	[Parameter()]
+	[System.Management.Automation.Language.CommandAst]
+	$commandAst,
+	
+	[Parameter()]
+	[System.Collections.IDictionary]
+	$fakeBoundParameters
+)
+process {
+	trap{break}
+	$FilePath = "{0}"
+
+	if (!(Test-Path $FilePath -PathType Leaf)) {
+		return
+	}
+
+	try {
+		[System.IO.FileStream]$FileStream = New-FileStream -FilePath $FilePath -FileMode Open -FileAccess Read
+		[System.IO.StreamReader]$FileReader = [System.IO.StreamReader]::new($FileStream)
+		while (!$FileReader.EndOfStream) {
+			$line = $FileReader.ReadLine()
+			if ("$wordToComplete*" -like $line) {
+				New-CompletionResult -CompletionText $line
+			}
+		}
+	}
+	finally {
+		if ($null -ne $FileStream) { $FileStream.Dispose() }
+		if ($null -ne $FileReader) { $FileReader.Dispose() }
+	}
+	
+}
