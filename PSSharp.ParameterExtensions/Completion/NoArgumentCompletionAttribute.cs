8b3d5f03dd7c90e0e67ff156fc4bfc2e61f82fff
Caleb Frederickson <caleb.frederickson@gmail.com>
2021-03-31 21:08:13 -0800

    add [NoArgumentCompletion()]

---
+++ PSSharp.ParameterExtensions/Completion/NoArgumentCompletionAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Text;
+
+namespace PSSharp
+{
+    /// <summary>
+    /// Indicates that no argument completion should be offered. This may be preferred
+    /// when ideal argument completion is not possible and a path argument completion
+    /// is not desired.
+    /// </summary>
+    public class NoArgumentCompletionAttribute : ArgumentCompleterAttribute, IArgumentCompleter
+    {
+        /// <summary>
+        /// Indicates that no argument completion should be offered.
+        /// </summary>
+        public NoArgumentCompletionAttribute()
+            : base(typeof(NoArgumentCompletionAttribute))
+        {
+        }
+        /// <summary>
+        /// Returns nothing.
+        /// </summary>
+        public IEnumerable<CompletionResult> CompleteArgument(string commandName, string parameterName, string wordToComplete, CommandAst commandAst, IDictionary fakeBoundParameters)
+        {
+            yield break;
+        }
+    }
+}
